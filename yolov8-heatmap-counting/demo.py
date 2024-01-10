0c2739b752370a47c9482713615f0c0940646241
PrashantDixit-dev <prasantdixit987@gmail.com>
2024-01-10 10:20:47 +0530

    heatmap count

--- yolov8-heatmap-counting/demo.py
+++ yolov8-heatmap-counting/demo.py
@@ -25,18 +25,20 @@ video_writer = cv2.VideoWriter("heatmap_output.mp4",
 # Heatmap init
 heatmap_obj = heatmap.Heatmap()
 
-heatmap_obj.set_args(colormap=cv2.COLORMAP_JET,
+heatmap_obj.set_args(colormap=cv2.COLORMAP_TURBO,
                      imw=cap.get(4),  # should same as im0 width
                      imh=cap.get(3),  # should same as im0 height
                      view_img=False,
                      count_reg_pts=count_reg_pts)
 
-while cap.isOpened(): 
+while True: 
     ret, frame = cap.read() 
     if(ret): 
         results = model.track(frame, persist=True)
         frame = heatmap_obj.generate_heatmap(frame, tracks=results)
         video_writer.write(frame)
+    else:
+        break
          
 video_writer.release()
 cap.release()
